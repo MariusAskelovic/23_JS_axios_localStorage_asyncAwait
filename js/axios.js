a9ed91f38ca756d96b6eea1ba79c2db1c7f3973a
MariusAskelovic <marashz@gmail.com>
2023-07-27 08:56:18 +0300

    init2

--- js/axios.js
+++ js/axios.js
@@ -1,11 +1,2 @@
 'use strict';
 console.log('axios.js file was loaded');
-
-const url = 'https://jsonplaceholder.typicode.com/users';
-
-fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch(console.warn);
