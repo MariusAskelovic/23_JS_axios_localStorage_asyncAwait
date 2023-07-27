bbde0ecba8e45f38e2a845493ed3ade737a56965
MariusAskelovic <marashz@gmail.com>
2023-07-27 11:02:10 +0300

    local storage

---
+++ js/localStorage.js
@@ -0,0 +1,19 @@
+'use strict';
+console.log('localStorage.js file was loaded');
+
+const els = {
+  num1: document.getElementById('num1'),
+  btn1: document.getElementById('btn1'),
+};
+
+let sk1Storage = localStorage.getItem('sk1');
+if (sk1Storage === null) {
+  sk1Storage = 0;
+}
+let numVal = Number(sk1Storage);
+els.num1.textContent = numVal;
+els.btn1.addEventListener('click', () => {
+  numVal = numVal + 5;
+  els.num1.textContent = numVal;
+  localStorage.setItem('sk1', numVal);
+});
