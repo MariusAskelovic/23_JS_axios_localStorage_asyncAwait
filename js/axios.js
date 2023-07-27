4103de8a38b4bbe52bc42b0ed368e171f21bcc77
MariusAskelovic <marashz@gmail.com>
2023-07-27 09:30:11 +0300

    axios get

--- js/axios.js
+++ js/axios.js
@@ -1,12 +1,27 @@
 'use strict';
 console.log('axios.js file was loaded');
-const url = 'https://jsonplaceholder.typicode.com/users';
+const url = 'https://jsonplaceholder.typicode.com/users111';
 
 fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => {
-    console.log(data);
+  .then((resp) => {
+    console.log('FETCH resp ===', resp);
+    // if (!resp.ok) throw new Error('kazkas ne taip');
+    return resp.json();
+  })
+  .then((atsInJs) => {
+    console.log('FETCH atsInJs === ', atsInJs);
   })
   .catch((error) => {
-    console.warn('ivyko klaida:', error);
+    console.warn('FETCH ivyko klaida:', error);
+  });
+
+axios
+  .get(url)
+  .then((axiosResp) => {
+    console.log('AXIOS resp ===', axiosResp);
+    const users = axiosResp.data;
+    // console.log('users ===', users);
+  })
+  .catch((error) => {
+    console.warn('AXIOS ivyko klaida:', error);
   });
