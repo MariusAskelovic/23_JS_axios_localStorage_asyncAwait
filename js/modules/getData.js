6706434f6eeac9f5dcb1c744e40fe3e727516565
MariusAskelovic <marashz@gmail.com>
2023-07-27 11:46:47 +0300

    apiData

--- js/modules/getData.js
+++ js/modules/getData.js
@@ -1,23 +1,36 @@
 export function getData(url) {
-  const fetchResult = fetch(url)
+  const fetchResul = fetch(url)
     .then((resp) => {
       return resp.json();
     })
     .then((atsInJs) => {
+      // console.log('FETCH atsInJs ===', atsInJs);
       return atsInJs;
     })
     .catch((error) => {
       console.warn('FETCH ivyko klaida:', error);
     });
-  return fetchResult;
+  return fetchResul;
 }
 
 export async function getDataAsync(url) {
   try {
-    const respUsers = await fetch(url);
-    const atsInJs = await respUsers.json();
+    const resp = await fetch(url);
+    const atsInJs = await resp.json();
     return atsInJs;
   } catch (error) {
-    console.warn('klaida klaidele');
+    console.warn(error);
+  }
+}
+
+export async function apiData(url) {
+  try {
+    const resp = await fetch(url);
+    const result = await resp.json();
+    return [result, null];
+  } catch (error) {
+    // jei yra klaida
+    console.warn('klaida apiData fn', error);
+    return [null, error];
   }
 }
