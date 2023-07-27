7621cdd7a0be807e000e81dec1c4b43d318685c4
MariusAskelovic <marashz@gmail.com>
2023-07-27 09:22:07 +0300

    fetch way

--- js/axios.js
+++ js/axios.js
@@ -1,2 +1,12 @@
 'use strict';
 console.log('axios.js file was loaded');
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.warn('ivyko klaida:', error);
+  });
