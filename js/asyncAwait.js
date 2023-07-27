cd43e9e85ccaa685d1c53a5f21aecb7f781e8546
MariusAskelovic <marashz@gmail.com>
2023-07-27 10:20:26 +0300

    try catch

--- js/asyncAwait.js
+++ js/asyncAwait.js
@@ -2,6 +2,7 @@
 console.log('asyncAwait.js file was loaded');
 
 const url = 'https://jsonplaceholder.typicode.com/users';
+const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
 
 function fetchUsers() {
   fetch(url)
@@ -19,9 +20,21 @@ function fetchUsers() {
 
 async function fetchUsersA() {
   // await - palaukti kol grys atsakymas ir tik tada vykdyti kita eilute
-  const resp = await fetch(url);
-  console.log(resp);
-  const atsInJs = await resp.json();
-  console.log('atsInJs ===', atsInJs);
+
+  try {
+    const respUsers = await fetch(url);
+    const atsInJs = await respUsers.json();
+    const respPosts = await fetch(urlPosts);
+    const ats = await respPosts.json();
+    console.log('userArr === ', atsInJs, 'posts === ', ats.slice(0, 15));
+  } catch (error) {
+    console.log('FETCH await ivyko klaida: ', error);
+  }
 }
 fetchUsersA();
+
+// async function fetchPosts() {
+//   console.log('posts ===', ats);
+// }
+
+// fetchPosts();
